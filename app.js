ccc4ffb3d64d130de667310bc7380fbd78a96adb
Flavio Garrido <fbonnin@teceng-gaming.com.ar>
2022-02-05 15:51:55 -0300

    Ex13.4 GET all, ADD and Remove

---
+++ app.js
@@ -0,0 +1,80 @@
+/* eslint-disable no-undef */
+require('dotenv').config()
+const { Sequelize, Model ,DataTypes } = require('sequelize')
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions:{
+    ssl:{
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+})
+
+class Blog extends Model {}
+Blog.init({
+  id:{
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  author:{
+    type: DataTypes.STRING(200),
+    allowNull: true
+  },
+  url:{
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  title:{
+    type: DataTypes.STRING(200),
+    allowNull: false
+  },
+  likes:{
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 0
+  }
+
+},{
+  sequelize,
+  underscored: true,
+  timestamps:false,
+  modelName: 'blogs'
+})
+
+app.get('/api/blogs', async(req, res) => {
+  const blogs = await Blog.findAll()
+  res.json(blogs)
+})
+
+app.post('/api/blogs', async(req, res) => {
+  try{
+    const blog = await Blog.create(req.body)
+    res.json(blog)
+  }catch(error){
+    return res.status(400).json({ error })
+  }
+})
+
+app.delete('/api/blogs/:id', async(req, res) => {
+  try{
+    await Blog.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    res.sendStatus(202)
+  }catch(error){
+    return res.status(400).json({ error })
+  }
+})
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
