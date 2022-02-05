ddefa1b8f6c0cb0239e348ebd09f33bc08a30895
Flavio Garrido <fbonnin@teceng-gaming.com.ar>
2022-02-05 16:41:03 -0300

    Ex13.6 Update likes function

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,20 +2,24 @@ const router = require('express').Router()
 
 const { Blog } = require('../models')
 
+const getBlog = async(req,res,next) => {
+  req.blog = await Blog.findByPk(req.params.id)
+  next()
+}
 
 router.get('/', async(req, res) => {
   const blogs = await Blog.findAll()
   res.json(blogs)
 })
 
-router.get('/:id', async(req, res) => {
-  try{
-    const blog = await Blog.findByPk(req.params.id)
-    res.json(blog)
-  }catch(error){
-    return res.status(400).json({ error })
+router.get('/:id',getBlog, async(req, res) => {
+  if(req.blog){
+    res.json(req.blog)
+  }else{
+    return res.status(404).end()
   }
 })
+
 router.post('/', async(req, res) => {
   try{
     const blog = await Blog.create(req.body)
@@ -25,16 +29,24 @@ router.post('/', async(req, res) => {
   }
 })
 
-router.delete('/:id', async(req, res) => {
-  try{
-    await Blog.destroy({
-      where: {
-        id: req.params.id
-      }
-    })
+router.delete('/:id', getBlog, async(req, res) => {
+  if(req.blog){
+    req.blog.destroy()
     res.sendStatus(202)
-  }catch(error){
-    return res.status(400).json({ error })
+  }else{
+    res.sendStatus(404)
+  }
+})
+
+router.put('/:id', getBlog, async(req,res) => {
+
+  if(req.blog){
+    req.blog.likes = req.body.likes
+    req.blog.save()
+    res.json(req.blog)
+  }else{
+    console.log(1)
+    res.sendStatus(404)
   }
 })
 
