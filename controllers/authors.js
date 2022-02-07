88696c3f54cedd80495613e9c2e2aff23a2334ac
Flavio Garrido <fbonnin@teceng-gaming.com.ar>
2022-02-06 22:10:39 -0300

    Ex13.16 Agregation fm

---
+++ controllers/authors.js
@@ -0,0 +1,18 @@
+const router = require('express').Router()
+const { Blog } = require('../models')
+const { sequelize } = require('../models/blog')
+
+router.get('/', async (req,res) => {
+  const authors = await Blog.findAll({
+    attributes: [
+      'author',
+      [sequelize.fn('COUNT', sequelize.col('id')), 'articles'],
+      [sequelize.fn('SUM', sequelize.col('likes')), 'likes'],
+    ],
+    group: 'author'
+  })
+
+  res.json(authors)
+})
+
+module.exports = router
