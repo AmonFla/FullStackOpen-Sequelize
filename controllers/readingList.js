304a02b218239bb3bb52980748f24a2bee6bcd35
Flavio Garrido <fbonnin@teceng-gaming.com.ar>
2022-02-08 08:55:06 -0300

    Ex13.22 update own readinglist read state

--- controllers/readingList.js
+++ controllers/readingList.js
@@ -1,10 +1,23 @@
 const router = require('express').Router()
+const middleware = require('../util/middleware')
 
 const { ReadingList } = require('../models')
 
 router.post('/', async(req, res) => {
   const reading = await ReadingList.create({ ...req.body })
   res.json(reading)
+})
+
+router.put('/:id', middleware.userExtractor, async(req,res) => {
+
+  const reading = await ReadingList.findByPk(req.params.id)
+  if (reading.userId === req.user){
+    reading.read = req.body.read
+    reading.save()
+    res.sendStatus(202)
+  }else{
+    res.sendStatus(401)
+  }
 })
 
 module.exports = router
